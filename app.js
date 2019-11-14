2a15a55f445edeb40c7599fd07c9356c38337df2
datsuny <43563262+datsuny@users.noreply.github.com>
2019-11-14 17:00:00 -0600

    Delete app.js

--- app.js
+++
@@ -1,8 +0,0 @@
-animar = () =>{
-    //.toggle() lo que hace es quita o eliminar clases de CSS o bootstrap
-    document.getElementById("barrita").classList.toggle("progress-bar-animated");
-}
-
-document.getElementById("boton").onclick = function (){
-    animar();
-}
