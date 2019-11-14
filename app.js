6dfb2db4047e135a1dbd3680735594a9c105cef3
datsuny <43563262+datsuny@users.noreply.github.com>
2019-11-14 12:07:33 -0600

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-function animar(){
+animar = () =>{
     //.toggle() lo que hace es quita o eliminar clases de CSS o bootstrap
     document.getElementById("barrita").classList.toggle("progress-bar-animated");
 }
